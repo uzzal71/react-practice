ee6c3d4ee45e01188c117a32a7ed4c3221497c71
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-06 23:48:48 +0600

    import all componet in app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import ContactForm from "./component/ContactForm";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+
 const App = () => {
   return (
     <div>
-      
+      <Header/>
+      <Hero/>
+      <ContactForm/>
+      <Footer/>
     </div>
   );
 };
