e395cea87916dbba0022a1b6fee6e525e8a84a39
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-07 22:56:45 +0600

    if conditions

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import Header from "./component/Header";
 import Hero from "./component/Hero";
 
 const App = () => {
+  let marks=80;
   return (
     <div>
+    <div>
+      {
+        marks>80?
+        <h1>Brilliant Result</h1>
+        :
+        <h1>Avarage Result</h1>
+      }
+    </div>
       <Header/>
       <Hero/>
       <ContactForm/>
