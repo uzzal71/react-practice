c1d6a60405c0ee23b109d8785bd6d8cbb37ba52c
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-07 23:57:20 +0600

    logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,17 @@
-import ContactForm from "./component/ContactForm";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
-import Hero from "./component/Hero";
-
 const App = () => {
   let marks=80;
   let cities=['Dhaka','London','Delhi','Kolkata'];
+  const status=true;
+  const LoginStatusBtn=(status)=>{
+    if (status) return <button>Logout Btn</button>
+    else return <button>Login Btn</button>
+  }
   return (
     <div>
-    <div>
-      {
-        marks>80?
-        <h1>Brilliant Result</h1>
-        :
-        <h1>Avarage Result</h1>
-      }
+      <h1>Login status</h1>
+      {LoginStatusBtn(false)}
     </div>
-    <div>
-      {(() => {
-        if (marks > 80) {
-          return <h1>Result: Brilliant</h1>
-        } else {
-          return <h1>Result: Avarage</h1>
-        }
-      })()}
-    </div>
-    <div>
-      <ol>
-        {
-          cities.map((city, i) => {
-            return <li key={i.toString()}>{city}</li>
-          })
-        }
-      </ol>
-    </div>
-      <Header/>
-      <Hero/>
-      <ContactForm/>
-      <Footer/>
-    </div>
-  );
+  )
 };
 
 export default App;
