00dbb5cb2b81d0d6f48b1cec63d0c6b2676493af
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-08 23:58:32 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useRef } from "react";
 
 const App = () => { 
-  let firstNmae = useRef();
-  let lastName = useRef();
-  
+  let firstNmae, lastName = useRef();
   const change = () => {
-    let fName = firstNmae.current.value;
-    let lName = lastName.current.value;
+    let fName = firstNmae.value;
+    let lName = lastName.value;
 
     alert(fName + " " + lName);
   }
   
   return (
     <div>
-      <input ref={firstNmae} placeholder="First Name"/><br/>
-      <input ref={lastName} placeholder="Last Name"/><br/>
+      <input ref={(a)=>firstNmae=a} placeholder="First Name"/><br/>
+      <input ref={(a)=>lastName=a} placeholder="Last Name"/><br/>
       <button onClick={()=>change()}>Click</button>
     </div>
   );
