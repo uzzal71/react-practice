f7739c3e2263e9abc0d74af9337d41077609cdc7
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-09 00:15:09 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useRef } from "react";
 
 const App = () => { 
-  let myHeadLine = useRef();
+  let demoRef = useRef(0);
   const change = () => {
-    myHeadLine.classList.add('text-danger');
-    myHeadLine.classList.remove('text-success');
+    demoRef.current++;
+    console.log(`Clicked ${demoRef.current} times`);
   }
   
   return (
     <div>
-      <h1 className="text-success" ref={(h1)=>myHeadLine=h1}>This is head line</h1>
+      <h1></h1>
       <button onClick={()=>change()}>Click</button>
     </div>
   );
