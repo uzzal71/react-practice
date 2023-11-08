d5645762123870985adec6a0206b0146e800a2fd
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-08 23:54:03 +0600

    useRef form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useRef } from "react";
 
 const App = () => { 
-  let demoRef = useRef();
+  let firstNmae, lastName = useRef();
   const change = () => {
-    demoRef.current.src="https://placehold.co/400"
-    demoRef.current.setAttribute("height", "200px");
-    demoRef.current.setAttribute("width", "200px");
+    
   }
   
   return (
     <div>
-      <img src="https://placehold.co/600x400" ref={demoRef}/>
-      <br/>
+      <input ref={firstNmae} placeholder="First Name"/><br/>
+      <input ref={lastName} placeholder="Last Name"/><br/>
       <button onClick={()=>change()}>Click</button>
     </div>
   );
