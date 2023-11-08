13c9d8a3b0d4588e365f70b67b80bf717162bc35
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-09 00:07:45 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useRef } from "react";
 
 const App = () => { 
-  let firstNmae, lastName = useRef();
+  let myHeadLine = useRef();
   const change = () => {
-    let fName = firstNmae.value;
-    let lName = lastName.value;
-
-    alert(fName + " " + lName);
+    myHeadLine.classList.add('text-danger');
+    myHeadLine.classList.remove('text-success');
   }
   
   return (
     <div>
-      <input ref={(a)=>firstNmae=a} placeholder="First Name"/><br/>
-      <input ref={(a)=>lastName=a} placeholder="Last Name"/><br/>
+      <h1 className="text-success" ref={(h1)=>myHeadLine=h1}>This is head line</h1>
       <button onClick={()=>change()}>Click</button>
     </div>
   );
