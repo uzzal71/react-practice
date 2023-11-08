b8a7684c5c9ddef697c50eec080e3ab4cdc0be32
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-08 21:30:32 +0600

    props function pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./component/Hero";
+import MyButton from "./component/MyButton";
 import ProductList from "./component/ProductList";
 
 const App = () => { 
@@ -9,10 +10,14 @@ const App = () => {
     price: 19.99,
     category: "Category 1"
   }
+  const handleClick = () => {
+    alert("Button clicked!");
+  }
   return (
     <div>
       <Hero title="Learn react" description="In details how to pass props"/>
       <ProductList Item={ItemObj}/>
+      <MyButton handleClick={handleClick}/>
     </div>
   );
 };
---
+++ src/component/MyButton.jsx
@@ -0,0 +1,9 @@
+const MyButton = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleClick}>Click</button>
+        </div>
+    );
+};
+
+export default MyButton;
