c89081f752c8dfcbfb615fb25884e3336e87dcb0
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-09 12:54:07 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import { useRef } from "react";
 
 const App = () => { 
   let expensiveResultRef = useRef(null);
-  let myDivRef = useRef(null);
+  let myDivRef = useRef();
 
   const fetchData = async () => {
-    let response = await fetch('https://dummyjson.com/products');
+    let response = await fetch("https://dummyjson.com/products");
     expensiveResultRef.current = await response.json();
   }
 
   const ShowData = () => {
-    myDivRef.current.innerHTML = JSON.stringfy(expensiveResultRef.current);
+    myDivRef.current.innerHTML = JSON.stringify(expensiveResultRef.current);
   }
   
   
   return (
     <div>
       <div ref={myDivRef}></div>
-      <button onClick={ShowData}>Call Api</button>
-      <button onClick={fetchData}>Show Div</button>
+      <button onClick={fetchData}>Call Api</button>
+      <button onClick={ShowData}>Show Data</button>
     </div>
   );
 };
