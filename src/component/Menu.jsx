d79f202fce9dd26ad49e1c6f7959fd20125b338e
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-11 01:05:19 +0600

    react-router-dom

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
     return (
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/product">Product</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
+                <li><NavLink className={({isActive})=>isActive?"active-item":"pending-item"} to="/">Home</NavLink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"pending-item"} to="/product">Product</NavLink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"pending-item"} to="/profile">Profile</NavLink></li>
             </ul>
         </div>
     );
