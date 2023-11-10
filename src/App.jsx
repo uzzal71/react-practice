56b3a6f541eb1f4210d238fe0793e147969d659a
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-11 00:24:40 +0600

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 
 const App = () => {
-  let [data, setData]=useState([]);
-
-  useEffect(()=>{
-    (async () => {
-      const response = await fetch('https://dummyjson.com/products/1');
-      let result = await response.json();
-      setData(result);
-    })()
-  },[]);
   
   return (
     <div className="container">
-      {JSON.stringify(data)}
+      <h1>React router dom</h1>
     </div>
   );
 };
