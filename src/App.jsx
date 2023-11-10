b8485a77128e50e98e203ac1631d4fc4ff7253cb
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-11 00:52:00 +0600

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFound from './pages/NotFound';
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   
   return (
-    <div className="container">
-      <h1>React router dom</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/product" element={<ProductPage/>}/>
+          <Route path="/profile" element={<ProfilePage/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
