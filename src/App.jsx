b0c2cbe0bba496da992763442171d70735dbbd56
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-10 20:18:54 +0600

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import { useState } from "react";
 
-const App = () => { 
+const App = () => {
   let [list, setList] = useState([]);
   let [item, setItem] = useState("");
 
-  const AddToList=()=>{
-    list.push(item);
-    setList([...list]);
-    setItem("");
-  }
+  const AddToList = () => {
+    if (item.trim() !== "") {
+      setList([...list, item]);
+      setItem("");
+    }
+  };
 
-  const RemoveItem=(index)=>{
-    list.pop(index);
+  const RemoveItem = (index) => {
+    list.splice(index, 1);
     setList([...list]);
-  }
+  };
 
   return (
     <div>
       <ul>
-        {
-          list.length > 0 ? list.map((element, index)=> {
-            return <li key={index}>{element} 
-            <span style={{ cursor: "pointer", color: "red" }} onClick={()=>{RemoveItem(index)}}>Remove</span>
+        {list.length > 0 ? (
+          list.map((element, index) => (
+            <li key={index}>
+              {element}
+              <span
+                style={{ cursor: "pointer", color: "red" }}
+                onClick={() => {
+                  RemoveItem(index);
+                }}
+              >
+                Remove
+              </span>
             </li>
-          }) : null
-        }
+          ))
+        ) : null}
       </ul>
-      <input onChange={(e)=>setItem(e.target.value)} placeholder="Item"/>
+      <input
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Item"
+      />
       <button onClick={AddToList}>Add</button>
     </div>
   );
