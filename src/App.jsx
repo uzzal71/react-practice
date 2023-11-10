619416f9038115d366f2a3765b49c8538f37003f
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-10 19:53:57 +0600

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import { useState } from "react";
 
 const App = () => { 
-  let [product, setProduct] = useState({
-    name: "Comfortable Business Wedding Office Men Genuine Leather dress shoes Derby Shoes",
-    price: 488.00,
-    discount: 10,
-    discount_type: "percent",
-    reviews:192,
-    rating: 4.92,
-    stock: 0
-  });
+  let [list, setList] = useState([]);
+  let [item, setItem] = useState({});
 
-  const updateProduct = () => {
-    setProduct(prevProduct=>({
-      ...prevProduct,
-      stock: 100
-    }));
-  }
-  
   return (
     <div>
-      <h3>{product.stock}</h3>
-      <button onClick={updateProduct}>update product</button>
+      <input placeholder="Item"/>
+      <button>Add</button>
     </div>
   );
 };
