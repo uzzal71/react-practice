46fae51587eed7841a5ba5c65539cfe0a169fcd3
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-11 01:17:44 +0600

    react-router-dom

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -6,8 +6,8 @@ const Menu = () => {
         <div>
             <ul>
                 <li><NavLink className={({isActive})=>isActive?"active-item":"pending-item"} to="/">Home</NavLink></li>
-                <li><NavLink className={(isActive)=>isActive?"active-item":"pending-item"} to="/product">Product</NavLink></li>
-                <li><NavLink className={(isActive)=>isActive?"active-item":"pending-item"} to="/profile">Profile</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive?"active-item":"pending-item"} to="/product">Product</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive?"active-item":"pending-item"} to="/profile">Profile</NavLink></li>
             </ul>
         </div>
     );
