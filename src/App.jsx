0ddf700b47316896a63d2a4cfaf3dbd3529c2e9f
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-10 20:37:45 +0600

    working with form

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  let [list, setList] = useState([]);
-  let [item, setItem] = useState("");
-
-  const AddToList = () => {
-    if (item.trim() !== "") {
-      setList([...list, item]);
-      setItem("");
-    }
-  };
-
-  const RemoveItem = (index) => {
-    list.splice(index, 1);
-    setList([...list]);
-  };
+  let [FormValue, SetFormValue]=useState({
+    fname:"",lname:"",city:"",gender:""
+  });
+  const InputOnChange=(InputName,InputValue)=>{
+    
+  }
 
   return (
     <div>
-      <ul>
-        {list.length > 0 ? (
-          list.map((element, index) => (
-            <li key={index}>
-              {element}
-              <span
-                style={{ cursor: "pointer", color: "red" }}
-                onClick={() => {
-                  RemoveItem(index);
-                }}
-              >
-                Remove
-              </span>
-            </li>
-          ))
-        ) : null}
-      </ul>
-      <input
-        value={item}
-        onChange={(e) => setItem(e.target.value)}
-        placeholder="Item"
-      />
-      <button onClick={AddToList}>Add</button>
+      
+      <button>Add</button>
     </div>
   );
 };
