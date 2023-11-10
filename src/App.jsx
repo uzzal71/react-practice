7326573727d66ce70ee3ca135811f00bdf6bd751
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-10 20:11:26 +0600

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 
 const App = () => { 
   let [list, setList] = useState([]);
-  let [item, setItem] = useState({});
+  let [item, setItem] = useState("");
+
+  const AddToList=()=>{
+    list.push(item);
+    setList([...list]);
+    setItem("");
+  }
+
+  const RemoveItem=(index)=>{
+    list.pop(index);
+    setList([...list]);
+  }
 
   return (
     <div>
-      <input placeholder="Item"/>
-      <button>Add</button>
+      <ul>
+        {
+          list.length > 0 ? list.map((element, index)=> {
+            return <li key={index}>{element} 
+            <span style={{ cursor: "pointer", color: "red" }} onClick={()=>{RemoveItem(index)}}>Remove</span>
+            </li>
+          }) : null
+        }
+      </ul>
+      <input onChange={(e)=>setItem(e.target.value)} placeholder="Item"/>
+      <button onClick={AddToList}>Add</button>
     </div>
   );
 };
