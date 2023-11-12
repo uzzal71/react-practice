5a0ceaa3b16d201e9c25892a5676ce447ee0bb6e
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-12 20:24:27 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import NotFound from './pages/NotFound';
-import ProductPage from "./pages/ProductPage";
-import ProfilePage from "./pages/ProfilePage";
+import { useRef } from "react";
 
 const App = () => {
+  let counter = useRef(0);
   
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/product/:id/:name" element={<ProductPage/>}/>
-          <Route path="/profile/:facebookID/:youtubeID" element={<ProfilePage/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
-      </BrowserRouter>
+      <h1>Counter: {counter.current}</h1>
     </div>
   );
 };
