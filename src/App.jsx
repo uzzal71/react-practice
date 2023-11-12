acfb56bafd314cac2cdc23732f247fde596ada01
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-12 21:58:43 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useRef } from "react";
-
 const App = () => {
-  let myHeadLine = useRef();
 
-  const change = () => {
-    myHeadLine.innerHTML="<ul><li>A</li><li>B</li></ul>";
-  }
   
   return (
     <div>
-      <h1 ref={(h1) => myHeadLine=h1}></h1>
-      <button onClick={change}>Click</button>
+      
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'milligram/dist/milligram.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./assets/css/style.css";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
