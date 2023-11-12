e0479b8c626797696056ba03517608baf653ef02
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-12 21:43:58 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ const App = () => {
   let myHeadLine = useRef();
 
   const change = () => {
-    myHeadLine.current.innerHTML="<ul><li>A</li><li>B</li></ul>";
+    myHeadLine.innerHTML="<ul><li>A</li><li>B</li></ul>";
   }
   
   return (
     <div>
-      <h1 ref={myHeadLine}></h1>
+      <h1 ref={(h1) => myHeadLine=h1}></h1>
       <button onClick={change}>Click</button>
     </div>
   );
