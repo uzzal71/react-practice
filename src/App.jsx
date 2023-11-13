0cbf0eff4c15c650ab4f8155f9d09bba6e0d9f31
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-13 19:34:47 +0600

    api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import Quote from "./component/query/Quote";
 
 const App = () => {
@@ -7,9 +6,7 @@ const App = () => {
   return (
     <div>
       <h1>API Call</h1>
-      <Suspense fallback={<h1>Loading data...</h1>}>
         <Quote/>
-      </Suspense>
     </div>
   );
 };
