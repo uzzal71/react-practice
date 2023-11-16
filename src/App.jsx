58f34088f4b4669430e4b017ce90837307e20595
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-16 23:20:01 +0600

    adb

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { Suspense } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import Quote from "./component/query/Quote";
-
 const App = () => {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        suspense: true
-      }
-    }
-  })
   
   return (
     <div>
-      <h1>API Calling</h1>
-      <QueryClientProvider client={client}>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <Quote />
-          <Quote />
-        </Suspense>
-      </QueryClientProvider>
+      
     </div>
   );
 };
